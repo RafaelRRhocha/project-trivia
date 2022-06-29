2d4b5b1777b81282a1c418728410d613674f424f
Rafael Rocha <1rafaelrocha2@gmail.com>
2022-06-29 20:03:26 -0300

    Requisito 10 Finalizado

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
-import { createApiAction, createAnswerAction, createCountAction } from '../Redux/actions';
+import {
+  createApiAction,
+  createAnswerAction,
+  createCountAction,
+} from '../Redux/actions';
 import Timer from '../components/Timer';
 import '../Css/Game.css';
 import { readTimer, readUser } from '../localStorage';
@@ -39,19 +43,18 @@ class Game extends React.Component {
       easy: 1,
     };
     if (textContent === finalApi.correct_answer) {
-      sendCount(+'10' + (setTimer * difficulty[finalApi.difficulty]));
+      sendCount(+'10' + setTimer * difficulty[finalApi.difficulty]);
     }
     const verifyAction = true;
     changeHasAnswer(verifyAction);
-  }
+  };
 
   getRandomAnswers = () => {
     const { finalApi, hasAnswer, disable } = this.props;
-    console.log(hasAnswer);
     const answers = finalApi.incorrect_answers
       && finalApi.incorrect_answers.map((e, i) => (
         <button
-          className={ (!hasAnswer) ? 'black' : 'red' }
+          className={ !hasAnswer ? 'black' : 'red' }
           key={ i }
           type="button"
           data-testid={ `wrong-answer-${i}` }
@@ -61,25 +64,26 @@ class Game extends React.Component {
           {e}
         </button>
       ));
-    const answersPush = answers && answers.push(
-      <button
-        className={ (!hasAnswer) ? 'black' : 'green' }
-        type="button"
-        key="4"
-        data-testid="correct-answer"
-        onClick={ this.clickAnswers }
-        disabled={ disable }
-      >
-        {finalApi.correct_answer}
-      </button>,
-    );
+    const answersPush = answers
+      && answers.push(
+        <button
+          className={ !hasAnswer ? 'black' : 'green' }
+          type="button"
+          key="4"
+          data-testid="correct-answer"
+          onClick={ this.clickAnswers }
+          disabled={ disable }
+        >
+          {finalApi.correct_answer}
+        </button>,
+      );
     console.log(answersPush);
     const randomAnswers = answers && answers.sort(() => Math.random() - +'0.5');
     return randomAnswers;
   };
 
   render() {
-    const { finalApi } = this.props;
+    const { finalApi, hasAnswer } = this.props;
     const tok = JSON.stringify(readUser());
     const n63 = 63;
     const n67 = 67;
@@ -90,10 +94,25 @@ class Game extends React.Component {
           this.errorToken()
         ) : (
           <div>
-            <Timer />
-            <h1 data-testid="question-category">{finalApi.category}</h1>
-            <p data-testid="question-text">{finalApi.question}</p>
-            <div data-testid="answer-options">{this.getRandomAnswers()}</div>
+            {finalApi && (
+              <>
+                <Timer />
+                <div>
+                  <h1 data-testid="question-category">{finalApi.category}</h1>
+                  <p data-testid="question-text">{finalApi.question}</p>
+                  <div data-testid="answer-options">{this.getRandomAnswers()}</div>
+                </div>
+              </>
+            )}
+            {hasAnswer && (
+              <button
+                type="button"
+                data-testid="btn-next"
+                onClick={ this.getApiRequest }
+              >
+                Next
+              </button>
+            )}
           </div>
         )}
       </>
